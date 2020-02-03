10b3371c3830dd300f0556e6f2844bc440fb5269
DEStroyerll <theoffmouse@gmail.com>
2020-02-03 20:04:58 +0200

    Changed text for commit conflict

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("my first GIT repo");
-        System.out.println("NewSuperBranch");
+//        System.out.println("my first GIT repo");
+//        System.out.println("NewSuperBranch");
     }
 }
